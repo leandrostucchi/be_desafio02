95392608c252efe932958dd101a8e54490bc82bc
leandrostucchi <lstucchi@hotmail.com>
2023-12-26 13:39:23 -0300

    modificacion updProduct

--- js/productManager.js
+++ js/productManager.js
@@ -60,26 +60,29 @@ class productManager{
     }
 
     borrarArchivo = async() => {
-        await fs.unlink(this.path);
+        await fs.unlink(this.path)
+        ;
     }
 
     updProduct(tittle,description,price,thumbnail,code,stock,id,productos){
         let productsOld =productos;
-        let productsNew = [];
         let mensaje = this.validarDatos(tittle,description,price,thumbnail,code,stock);
-         if(mensaje == ''){
-            productsOld.push({tittle,description,price,thumbnail,code,stock,id});
+        if(mensaje == ''){
             for (let index = 0; index < productsOld.length; index++) {
-                productsNew.push(productsOld[index])
+                if(productsOld[index].id === id)    productsOld[index] = {tittle,description,price,thumbnail,code,stock,id};
              }
-             this.addFile(this.path,productsNew,'Se actualizo el registro');             
+             this.addFile(this.path,productsOld,'Se actualizo el registro');             
+        }else{
+            console.log(`Mensaje de error: ${mensaje}`);
         }
+
     }
 
-
     readProducts = async()=>{
-        let resultado = await fs.readFile(this.path,"utf-8");            
-        return JSON.parse(resultado);
+        let resultado = await fs.readFile(this.path,"utf-8")
+                                .catch(()=> console.log('No hay Datos'));            
+        if(resultado) return JSON.parse(resultado);
+        return '';
     }
 
     getProducts = async ()=> {
@@ -89,7 +92,7 @@ class productManager{
 
     readProductByID= async(id) => {
         let producto =  await this.readProducts();
-        return producto.some(x=> x.id === id) ?  producto.find(x=> x.id === id) : `Id Not Found ${id}`;
+        if (producto) return producto.some(x=> x.id === id) ?  producto.find(x=> x.id === id) : `Id Not Found ${id}`;
     }
 
     getExisteCodigo= (code) => {
@@ -99,12 +102,13 @@ class productManager{
 
     readProductWhitOutID= async(id) => {
         let producto =  await this.readProducts();
-        return producto.filter(x=> x.id !== id);
+        if (producto) return producto.filter(x=> x.id !== id);
+        return '';
     }
 
     getProductById = async(id) => {
         let producto =  await this.readProductByID(id);
-        console.log(producto);
+        if(producto) console.log(producto);
     }
 
     addFile = async (archivo,registro,mensaje)=>{
@@ -114,25 +118,15 @@ class productManager{
             .catch((err) => console.log(err))
     }
 
-    // updFile = async (archivo,registro,mensaje)=>{
-    //     let log =JSON.stringify(registro);
-    //     await fs.appendFile(archivo, log)
-    //         .then(() => console.log(`${mensaje}`))
-    //         .catch((err) => console.log(err))
-    // }
-
-
     deleteProduct = async(id) =>{
         let producto =  await this.readProductWhitOutID(id);
         if(producto) this.addFile(this.path,producto, `Elimino Id ${id}`);
     }
 
     updateProduct = async(tittle,description,price,thumbnail,code,stock,id) =>{        
-        this.deleteProduct(id);
         let productos =  await this.readProducts();
         await this.updProduct(tittle,description,price,thumbnail,code,stock,id,productos);
      }
-
 }
 
 
@@ -140,17 +134,19 @@ class productManager{
 let productoNuevo = new productManager();
 //productoNuevo.borrarArchivo();
 
-// productoNuevo.addProduct('producto prueba1','Este es un producto prueba1',200,'Sin imagen1','abc123',25);
-// productoNuevo.addProduct('producto prueba2','Este es un producto prueba2',200,'Sin imagen2','abc124',25);
-// productoNuevo.addProduct('producto prueba3','Este es un producto prueba3',200,'Sin imagen3','abc121',25);
-// productoNuevo.addProduct('producto prueba3','Este es un producto prueba3',200,'Sin imagen3','abc123',25);
+productoNuevo.addProduct('producto prueba1','Este es un producto prueba1',200,'Sin imagen1','abc123',25);
+productoNuevo.addProduct('producto prueba2','Este es un producto prueba2',200,'Sin imagen2','abc124',25);
+productoNuevo.addProduct('producto prueba3','Este es un producto prueba3',200,'Sin imagen3','abc121',25);
+productoNuevo.addProduct('producto prueba3','Este es un producto prueba3',200,'Sin imagen3','abc123',25); //codigo duplicado
 
-// productoNuevo.getProducts();
+//productoNuevo.getProducts();
 
-// productoNuevo.getProductById(1);
+//productoNuevo.getProductById(1);
 // productoNuevo.getProductById(5);
 
 //productoNuevo.deleteProduct(3);
 
 
-productoNuevo.updateProduct('producto prueba2','Este es un producto prueba2',400,'Sin','abc124',25,2);
+ //productoNuevo.updateProduct('producto prueba2','Este es un producto prueba2',400,'Sin','abc124',25,2);
+ //productoNuevo.updateProduct('prueba1','Este es un producto prueba1',200,'Sin imagen1','abc123',125,1);
+
